836d8eb45583285ba99e1f5746747eaefa06a0a6
sweenaaaaaaaaa <166064589+sweenaaaaaaaaa@users.noreply.github.com>
2026-08-26 15:19:28 +0800

    Remove obsolete Renyi build file

--- dist/client/assets/index-BEI5fS7P.js
+++
@@ -1,1 +0,0 @@
-import{a as t,j as r}from"./main-Bmn4gtTr.js";import{S as a}from"./site-mirror-page-CSUsqGaP.js";function m(){const{page:o}=t.useLoaderData();return r.jsx(a,{page:o,path:"/"})}export{m as component};
